d132944d1e94d8eead1bc11a4a78ae05cc45ff49
János Csőke <janos@csoke.eu>
2023-08-14 11:26:27 +0200

    Init commit

---
+++ random_client.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#	random-client
+#
+################## General section ##################
+function pause(){
+ read -p "$*"
+}
+
+#### COLORS ####
+RED='\033[0;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[1;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0m'
